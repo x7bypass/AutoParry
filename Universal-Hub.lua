00cdc6b551b13f485646287219649fda38d62aae
x7bypass <rrqtphoenix4@gmail.com>
2024-05-21 00:04:49 +0700

    Update Universal-Hub.lua

--- Universal-Hub.lua
+++ Universal-Hub.lua
@@ -105,11 +105,6 @@ Tab:AddButton({
 	Name = "W Azure",
         Icon = "rbxassetid://7733715400",
 	Callback = function()
-            getgenv().Team = "Marine"
-getgenv().AutoLoad = false --Will Load Script On Server Hop
-getgenv().SlowLoadUi  = false
-getgenv().ForceUseSilentAimDashModifier = false --Force turn on silent aim , if error then executor problem
-getgenv().ForceUseWalkSpeedModifier = false --Force turn on Walk Speed Modifier , if error then executor problem
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
     
   	end    
